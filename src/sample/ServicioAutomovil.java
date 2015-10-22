f8f3d5c440c3d404f2e509565e2211a157513f50
valdemar12 <val94rs@gmail.com>
2015-10-22 07:38:41 -0500

    Servicios  Taller

---
+++ src/sample/ServicioAutomovil.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "servicioautomovil")
+public class ServicioAutomovil implements Serializable{
+    @Id
+    @Column(name = "IDServicios")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer idservicios;
+    private Integer matricula;
+
+    public Integer getIdservicios(){
+        return idservicios;
+    }
+    public void setIdservicios(Integer idservicios){
+        this.idservicios=idservicios;
+    }
+    public Integer getMatricula(){
+        return matricula;
+    }
+    public void setMatricula(Integer matricula){
+        this.matricula=matricula;
+    }
+    public String toString(){
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("IDservicio:" + idservicios + ";");
+        return buffer.toString();
+    }
+}
