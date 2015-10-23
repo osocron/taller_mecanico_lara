dd724b782821e0a8c1f434e0138c6c18e1e91bc7
valdemar12 <val94rs@gmail.com>
2015-10-23 14:28:03 -0500

    Servicios  Taller 2.1.1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Taller mecanico");
+        primaryStage.setTitle("Taller mécanico");
         primaryStage.setScene(new Scene(root,700,500));
         primaryStage.show();
     }
