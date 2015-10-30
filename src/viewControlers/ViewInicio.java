21070ffb179fd9810ab1dd535a54cf8086b06433
osocron <osocron@gmail.com>
2015-10-29 21:04:52 -0600

    Progreso en Inicio View

--- src/viewControlers/ViewInicio.java
+++ src/viewControlers/ViewInicio.java
@@ -2,7 +2,11 @@ package viewControlers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,10 +18,49 @@ public class ViewInicio implements Initializable {
 
     @FXML
     public Button registroButton,ventaButton,cotizacionButton,controlButton;
+    public BorderPane rootBorderPane;
+    public ImageView imageView;
+
+    public Button registrarAutomovil,registrarCliente,registrarEmpleado,registrarProveedor,
+            registrarRefaccion,registrarServicio,registrarVenta;
+    public VBox vBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeElements();
+    }
 
+
+    public void displayRegistroOptions() {
+        rootBorderPane.getChildren().remove(imageView);
+        rootBorderPane.setCenter(vBox);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(registrarAutomovil,registrarCliente,registrarEmpleado,registrarProveedor,
+                registrarRefaccion,registrarServicio,registrarVenta);
+    }
+
+    public void displayVentasOptions() {
+
+    }
+
+    public void displayCotizacionesOptions() {
+
+    }
+
+
+    public void displayControlOptions() {
+
+    }
+
+    private void initializeElements(){
+        vBox = new VBox(20);
+        registrarAutomovil = new Button("Registrar Automovil");
+        registrarCliente = new Button("Registrar Clientes");
+        registrarEmpleado = new Button("Registrar Empleados");
+        registrarProveedor = new Button("Registrar Proveedores");
+        registrarRefaccion = new Button("Registrar Refacciones");
+        registrarServicio = new Button("Registrar Servicios");
+        registrarVenta = new Button("Registrar Ventas");
     }
 
 }
--- src/viewControlers/ViewRegistrarAutomovil.java
+++ src/viewControlers/ViewRegistrarAutomovil.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,9 +15,7 @@ public class ViewRegistrarAutomovil implements Initializable {
 
     @FXML
     public TextField matriculaTextField,marcaTextField,modeloTextField,colorTextField;
-    @FXML
     public ComboBox<ClienteEntity> clienteEntityComboBox;
-    @FXML
     public Button registrarButton;
 
     @Override
