30730ac133b52328b5d9523c22ec9a9f17fbad14
valdemar12 <val94rs@gmail.com>
2015-11-19 08:46:58 -0600

    pdf1.2.2.

--- src/venta/ViewImprimirCotizacion.java
+++ src/venta/ViewImprimirCotizacion.java
@@ -1,6 +1,10 @@
 package venta;
 
 import cliente.ControladorCliente;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.jfoenix.controls.JFXButton;
 import entidades.*;
 import javafx.collections.FXCollections;
@@ -14,7 +18,10 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import refaccion.ControladorRefaccion;
 import servicio.ControladorServicio;
+import viewControlers.DatePickerCell;
 
+import javax.swing.*;
+import java.io.FileOutputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -67,32 +74,33 @@ public class ViewImprimirCotizacion implements Initializable {
         tablaCotizacion.setEditable(true);
         tableCliente.setCellValueFactory(new PropertyValueFactory<>("cliente"));
         tableRefaccion.setCellValueFactory(new PropertyValueFactory<>("marca"));
-        tableRefaccion.setCellFactory(TextFieldTableCell.forTableColumn());
-        tableRefaccion.setOnEditCommit(event -> ControladorRefaccion.modificarMarca(event.getTableView()
-        .getSelectionModel().getSelectedItem().getIdRefaccion(),event.getNewValue()));
         tableServicio.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
-        tableServicio.setCellFactory(TextFieldTableCell.forTableColumn());
-        tableServicio.setOnEditCommit(event -> ControladorServicio.modificarDescripcion(event.getTableView()
-        .getSelectionModel().getSelectedItem().getIdServicio(),event.getNewValue()));
         tableFecha.setCellValueFactory(new PropertyValueFactory<>("fecha"));
-        tableFecha.setCellFactory(TextFieldTableCell.forTableColumn());
+        //tableFecha.setCellFactory(DatePickerCell.);
         /*tableFecha.setOnEditCommit(event -> ControladorVentas.modificarFecha(event.getTableView()
         .getSelectionModel().getSelectedItem().getIdVenta(),event.getNewValue()));*/
+
         //Tenemos que seleccionar el campo de costo del servicio en la tabla servicio
         tableCostoServicio.setCellValueFactory(new PropertyValueFactory<>("idVentas"));
-        tableCostoServicio.setCellFactory(TextFieldTableCell.forTableColumn());
-        /*tableCostoServicio.setOnEditCommit(event -> ControladorVentasServicio.modificarIDventas(event.getTableView()
-        .getSelectionModel().getSelectedItem().getIdVentas(), event.getNewValue()));*/
         tableCostoRefaccion.setCellValueFactory(new PropertyValueFactory<>("idRefacciones"));
-        tableCostoRefaccion.setCellFactory(TextFieldTableCell.forTableColumn());
-        /*tableCostoRefaccion.setOnEditCommit(event -> ControladorVentasRefaccion.modificarIDrefaccion(event.getTableView()
-        .getSelectionModel().getSelectedItem().getIdVentaRefaccion(), event.getNewValue()));*/
+
 
         tablaCotizacion.setItems(dataVentas);
 
     }
     public void imprimirActionEvent(){
+        Document doc = new Document();
+        try{
+            PdfWriter.getInstance(doc,new FileOutputStream("reporte.pdf"));
 
+            doc.open();
+            PdfPTable tabla = new PdfPTable(6);
+            tabla.addCell(tableCliente.getTypeSelector());
+            doc.add(tabla);
+            doc.close();
+        }catch (Exception e){
+
+        }
     }
     public void cancelarActionEvent(){
         textfieldSubtotal.setText("");
